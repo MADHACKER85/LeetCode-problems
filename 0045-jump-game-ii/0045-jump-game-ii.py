86b8bc2b37313b5dd78dd412b22f7f069a4183e4
KEERTHIVASAN K <109860552+MADHACKER85@users.noreply.github.com>
2024-11-16 15:36:04 +0530

    Time: 11 ms (50.87%), Space: 17.7 MB (37.12%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -3,15 +3,13 @@ class Solution:
     ans = 0
     end = 0
     farthest = 0
-
-    # Start an implicit BFS.
     for i in range(len(nums) - 1):
       farthest = max(farthest, i + nums[i])
       if farthest >= len(nums) - 1:
         ans += 1
         break
-      if i == end:      # Visited all the items on the current level.
-        ans += 1        # Increment the level.
-        end = farthest  # Make the queue size for the next level.
+      if i == end:      
+        ans += 1        
+        end = farthest  
 
     return ans
